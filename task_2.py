c65d9ed2fbbf4bd213bf6777f00cf681a1dee730
shaid03 <kancain00@gmail.com>
2024-03-11 16:10:30 +0500

    task_2

---
+++ task_2.py
@@ -0,0 +1,16 @@
+input_string = ' AI and related technologies have a positive impact on the way the IT sector works '
+
+# a
+print(input_string.upper())
+
+# b
+print(input_string.lower())
+
+# c
+print(input_string.strip())
+
+# d
+print(input_string.split())
+
+# e
+print(input_string.replace("a", "B"))
